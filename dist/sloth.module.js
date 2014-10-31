aabd9473d535aedaca37c2d82fbb695631c055d7
Pieter Beulque <pieter@mrhenry.be>
2014-10-31 12:39:10 +0100

    New build

--- dist/sloth.module.js
+++ dist/sloth.module.js
@@ -22,8 +22,7 @@ var sloth = {};
  */
 function Sloth($element, settings) {
   if (typeof $element === 'undefined') {
-    printError('Cannot initialize Sloth, no element given.');
-    return;
+    throw 'Cannot initialize Sloth, no element given.';
   }
 
   // settings
@@ -68,7 +67,6 @@ Sloth.prototype.init = function () {
 };
 
 
-
 /**
  * [Preloading]
  *
@@ -107,7 +105,6 @@ Sloth.prototype.onLoad = function ($img) {};
  * @context {Sloth} instance
  */
 Sloth.prototype.onError = function () {};
-
 
 
 /**
@@ -194,7 +191,6 @@ Sloth.prototype.parseOptions = function () {
 };
 
 
-
 /**
  * [Other]
  *
@@ -236,7 +232,6 @@ Sloth.prototype.unbind = function () {
 };
 
 
-
 /**
  * [@constructor level]
  *
@@ -268,26 +263,11 @@ Sloth.load = function (selector, settings) {
   $(selector).each(function () {
     if (this.tagName.toLowerCase() === 'img') {
       new sloth.Inline($(this), settings);
-
     } else {
       new sloth.Background($(this), settings);
-
     }
   });
 };
-
-
-
-/**
- * [Private]
- *
- * Print error
- */
-function printError(message) {
-  if (window.console && window.console.error) {
-    console.error(message);
-  }
-}
 
 
 /**
@@ -302,20 +282,25 @@ function printError(message) {
  * @return {String}
  */
 function defaultSourceGetterFunction(modifier) {
-  var src = this.$element.attr("data-src").split("/");
+  var src = this.$element.attr('data-src').split('/');
 
   // Replace current modifier
-  if ( src.length == 5 ) {
+  if (src.length === 5) {
     src[4] = modifier;
 
   // Append modifier
   } else {
     src.push(modifier);
-
   }
 
-  return src.join("/");
+  return src.join('/');
 }
+
+/**
+ * @namespace
+ */
+var sloth = sloth || {};
+
 
 /**
  * @constructor Sloth.Background
@@ -336,7 +321,6 @@ BackgroundSloth.prototype.constructor = sloth.Base;
  * @export
  */
 sloth.Background = BackgroundSloth;
-
 
 
 /**
@@ -364,8 +348,6 @@ BackgroundSloth.prototype.onError = function () {
     console.error('Could not load', this.source);
   } catch(err) {}
 };
-
-
 
 
 /**
@@ -421,6 +403,12 @@ BackgroundSloth.prototype.wrap = function () {
 };
 
 /**
+ * @namespace
+ */
+var sloth = sloth || {};
+
+
+/**
  * @constructor Sloth.Inline
  */
 function InlineSloth($element, settings) {
@@ -441,16 +429,15 @@ InlineSloth.prototype.constructor = sloth.Base;
 sloth.Inline = InlineSloth;
 
 
-
 /**
  * [Preloading]
  *
  * On load (inherits from sloth.base)
  */
 InlineSloth.prototype.onLoad = function ($img) {
-  var $wrapper = this.$element.closest('.sloth');
-  var assumed_height;
-  var orientation;
+  var $wrapper = this.$element.closest('.sloth'),
+      assumedHeight,
+      orientation;
 
   // set width to the actual css property or inherited width
   // instead of fixed placeholder
@@ -458,18 +445,15 @@ InlineSloth.prototype.onLoad = function ($img) {
 
   // compensate the difference between the assumed ratio
   // and the actual image height
-  assumed_height = (this.initialWidth / $img.width()) * $img.height();
+  assumedHeight = (this.initialWidth / $img.width()) * $img.height();
 
   // Get the orientation
-  if ( this.width / assumed_height == 1 ) {
+  if (this.width / assumedHeight == 1) {
     orientation = 'is-square';
-
-  } else if ( this.width / assumed_height < 1 ) {
+  } else if (this.width / assumedHeight < 1) {
     orientation = 'is-portrait';
-
   } else {
     orientation = 'is-landscape';
-
   }
 
   // fade in element
@@ -484,7 +468,7 @@ InlineSloth.prototype.onLoad = function ($img) {
   $wrapper
     .addClass(orientation)
     .animate({
-      height: assumed_height
+      height: assumedHeight
     }, 220, function () {
       $wrapper.css('height', '');
     });
@@ -519,21 +503,19 @@ InlineSloth.prototype.calculateDimensions = function () {
   // -- width
   if (!!width) {
     // element has specified width
-    this.initialWidth = width;
-    this.width = (this.$element.get(0).style.width !== '') ?
-      this.$element.get(0).style.width :
-      this.initialWidth;
+    this.initialWidth = this.width = width;
 
+    if (this.$element.get(0).style.width !== '') {
+      this.width = this.$element.get(0).style.width;
+    }
   } else {
     // inherit width
-    this.initialWidth = parseInt(this.$element.parent().css('width'));
-    this.width = this.initialWidth;
+    this.initialWidth = this.width = parseInt(this.$element.parent().css('width'));
 
     // when having a max-width specified, the image doesn't need a set width
     if (this.$element.css('max-width') !== 'none') {
       this.width = '';
     }
-
   }
 
   // -- height
@@ -546,21 +528,21 @@ InlineSloth.prototype.calculateDimensions = function () {
  */
 InlineSloth.prototype.wrap = function () {
   var $wrapper = $('<span class="sloth is-loading" />'),
-      wrapper_opts;
+      wrapperOptions;
 
-  wrapper_opts = {
+  wrapperOptions = {
     'width': this.initialWidth,
     'height': this.initialHeight,
     'display': 'inline-block',
     'font-size': 0
   };
 
-  if ( this.$element.css('max-width') !== 'none' ) {
-    $.extend(wrapper_opts, { 'max-width': this.$element.css('max-width') });
+  if (this.$element.css('max-width') !== 'none') {
+    $.extend(wrapperOptions, { 'max-width': this.$element.css('max-width') });
   }
 
   // take in the reserved space in the dom
-  $wrapper.css(wrapper_opts);
+  $wrapper.css(wrapperOptions);
 
   // since the wrapper took over the positioning of the image,
   // make the image fill the wrapper
